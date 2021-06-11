cd26e07d4903f55622a97708fbeb2396fdbb039c
Kian <kian@kian>
2021-06-12 00:53:00 +0100

    separate html template from python file

--- sitegen.py
+++ sitegen.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+print("Importing modules...")
+
 import datetime
 import jinja2
 import subprocess
@@ -10,36 +12,14 @@ def uptime_sec():
         uptime_seconds = float(f.readline().split()[0])
         return int(uptime_seconds)
 
+print("reading template...")
+with open('index.html', 'r') as the_file:
+    template = the_file.read()
+
+print("rendering...")
+
 uptime = str(datetime.timedelta(seconds=uptime_sec()))
 temp = subprocess.run(["/opt/vc/bin/vcgencmd", "measure_temp"], capture_output=True).stdout.decode('utf-8')[5:-1]
-
-template = """
-<html lang="en">
-<head>
-    <title>kians solar panel</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        html {
-            background: LemonChiffon;
-
-        }
-        body {
-            background: LemonChiffon;
-            color: #333;
-            padding: 2em 1em;
-            font-size: 2em;
-        }
-    </style>
-</head>
-<body>
-    <h1>kians solar panel</h1>
-    <p>temperature: {{ temp }}</p>
-    <p>uptime: {{ uptime }}</p>
-    <p>last updated: {{ time }} UTC</p>
-</body>
-</html>
-"""
 
 data = {
     "temp": temp,
@@ -48,6 +28,8 @@ data = {
 }
 rendered = jinja2.Template(template).render(data)
 
-#print(rendered)
+print("writing to file...")
 with open('/var/www/html/index.html', 'w') as the_file:
     the_file.write(rendered)
+
+print("done")
