efaceafb67808e7103a57c0a8ff5b6185d1c7f85
Kian <kian@kian>
2021-06-12 06:21:30 +0100

    uptime prettier format

--- sitegen.py
+++ sitegen.py
@@ -7,18 +7,13 @@ import jinja2
 import subprocess
 import time
 
-def uptime_sec():
-    with open('/proc/uptime', 'r') as f:
-        uptime_seconds = float(f.readline().split()[0])
-        return int(uptime_seconds)
-
 print("reading template...")
 with open('index.html', 'r') as the_file:
     template = the_file.read()
 
 print("rendering...")
 
-uptime = str(datetime.timedelta(seconds=uptime_sec()))
+uptime = subprocess.run(["/usr/bin/uptime", "-p"], capture_output=True).stdout.decode('utf-8')[3:]
 temp = subprocess.run(["/opt/vc/bin/vcgencmd", "measure_temp"], capture_output=True).stdout.decode('utf-8')[5:-1].replace('\'', '°')
 
 data = {
