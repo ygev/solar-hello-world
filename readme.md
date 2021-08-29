ba2f34e45da859ad9986cfdb45e4141133e7cce5
Kian <fafrd@users.noreply.github.com>
2021-08-29 06:37:44 +0100

    Sitegen now automates itself (rather than using cron or watch)

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # solar panel powered webserver
 
 ## how it works
-Every ~5 seconds, the ./sitegen.py file is run, generating a new static html file. This file is then served by nginx.
+Every ~5 seconds, sitegen.py will generate a new static html file. This file is then served by nginx.
 
 ## view it live - [solar.kian.club](http://solar.kian.club)
--- sitegen.py
+++ sitegen.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python
 
-print("Importing modules...")
+print("Starting sitegen")
 
 import datetime
 import jinja2
 import subprocess
 import time
 
-print("reading template...")
-with open('index.html', 'r') as the_file:
-    template = the_file.read()
+while True:
+    try:
+        print("reading template...")
+        with open('index.html', 'r') as the_file:
+            template = the_file.read()
 
-print("rendering...")
+        print("rendering...")
 
-uptime = subprocess.run(["/usr/bin/uptime", "-p"], capture_output=True).stdout.decode('utf-8')[3:]
-temp = subprocess.run(["/opt/vc/bin/vcgencmd", "measure_temp"], capture_output=True).stdout.decode('utf-8')[5:-1].replace('\'', '°')
+        uptime = subprocess.run(["/usr/bin/uptime", "-p"], capture_output=True).stdout.decode('utf-8')[3:]
+        temp = subprocess.run(["/opt/vc/bin/vcgencmd", "measure_temp"], capture_output=True).stdout.decode('utf-8')[5:-1].replace('\'', '°')
 
-data = {
-    "temp": temp,
-    "uptime": uptime,
-    "time": time.ctime()
-}
-rendered = jinja2.Template(template).render(data)
+        data = {
+            "temp": temp,
+            "uptime": uptime,
+            "time": time.ctime()
+        }
+        rendered = jinja2.Template(template).render(data)
 
-print("writing to file...")
-with open('/var/www/html/index.html', 'w') as the_file:
-    the_file.write(rendered)
+        print("writing to file...")
+        with open('/var/www/html/index.html', 'w') as the_file:
+            the_file.write(rendered)
 
-print("done")
+        print("done")
+        time.sleep(5)
+    except Exception as e:
+        print(e)
+        time.sleep(30)
